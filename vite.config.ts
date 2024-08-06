612c7c712ddb9ba25bdb717c401dd44432c8004e
Vivek Kalyan <hello@vivekkalyan.com>
2024-08-06 18:00:46 +0800

    build: Add base property

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/llm-cost-calculator/",
   plugins: [react()],
   resolve: {
     alias: {
